301ce2485c478ae8b5711969ab193feddc51f85d
Administrator <m-sh@qq.com>
2020-01-01 00:29:20 +0800

    引用类依赖

--- eurka-server/src/main/java/com/msh/eurka/server/EurkaServerApplication.java
+++ eurka-server/src/main/java/com/msh/eurka/server/EurkaServerApplication.java
@@ -1,5 +1,9 @@
 package com.msh.eurka.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 @SpringBootApplication
 @EnableEurekaServer
 public class EurkaServerApplication {
--- gateway-server/src/main/java/com/msh/gateway/server/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/msh/gateway/server/GatewayServerApplication.java
@@ -1,10 +1,13 @@
 package com.msh.gateway.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 @SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient
 public class GatewayServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run( EntoreGatewayApplication.class, args );
+        SpringApplication.run( GatewayServerApplication.class, args );
     }
 }
